2b698cb6e3aa20217adf03fb3282246eeb4e9534
naval620 <m01141721f@edu.teu.ac.jp>
2016-12-19 02:12:23 +0900

    掲示板改二

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20161218074654) do
 
-  create_table "blogs", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "content",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.datetime "created_at",             null: false
